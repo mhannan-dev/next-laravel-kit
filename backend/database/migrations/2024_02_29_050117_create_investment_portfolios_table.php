4c7c1d3a0050ddcfba034a12ce726e00677dae37
Muhammad Hannan <mdhannan.info@gmail.com>
2024-03-29 11:42:34 +0600

    Ready for FinVue project

---
+++ backend/database/migrations/2024_02_29_050117_create_investment_portfolios_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('investment_portfolios', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('portfolio_name');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('investment_portfolios');
+    }
+};
